9cff0480a1997a3b0cfd765873d5788a3f33cf68
James Loveridge <dev@j-m.es>
2022-03-01 22:40:42 +0000

    Add .config/oh-my-zsh/exports.zsh

---
+++ .config/oh-my-zsh/exports.zsh
@@ -0,0 +1,10 @@
+#export HOMEBREW_CASK_OPTS=--require-sha
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.fnm:$PATH
+export PATH=/opt/homebrew/bin:$PATH
+export PATH=/opt/homebrew/sbin:$PATH
+export PATH=node_modules/.bin:$PATH
+
+export GPG_TTY=$(tty)
